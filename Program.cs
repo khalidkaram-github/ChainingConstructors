88bc76f2ce093581fd296e229ca6197d5555da12
khalid karam <khalid.tasks@gmail.com>
2024-09-09 12:23:01 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace ChainingConstructors
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var user = new User();
+            Console.WriteLine($"Name={user.Name} , age ={user.Age}");
+        }
+    }
+    class User
+    {
+        public int Age { get; set; }
+        public string Name { get; set; }
+
+        public User() : this(10, "ahmed")
+        {
+
+        }
+        public User(int age) : this(age, "ahmed")
+        {
+
+        }
+        public User(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+
+}
